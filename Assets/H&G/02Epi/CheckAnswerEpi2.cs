eacd15e6b03cf762734ea6d4d74d7da99dc89931
ByungKwon Lee <88296556+byungkwon2023F@users.noreply.github.com>
2023-10-29 17:09:12 -0400

    Update CheckAnswerEpi2.cs

--- Assets/H&G/02Epi/CheckAnswerEpi2.cs
+++ Assets/H&G/02Epi/CheckAnswerEpi2.cs
@@ -1,58 +1,58 @@
-﻿/*
- * - Name : CheckAnswerEpi2.class
- * - Writer : 유희수
- * - Content : 헨젤과그레텔 Epi2 미니게임 - 몇개의 퍼즐조각이 맞춰졌는지 세고, 모두 맞춰지면 다음씬으로 이동하는 스크립트
- *  
- * - HISTORY
- * 2021-08-10 : 초기 개발
- * 2021-08-12 : 코드 획일화 및 주석처리
- *
- * <Variable>
- * mn_count                     퍼즐이 몇개 맞춰졌는지 확인을 위한 변수
- * v_YouWinText                 게임이 끝났음을 표시해주는 텍스트
- * 
- * <Function>
- * v_CountAnswer()              퍼즐을 맞출때마다 개수 변수를 더해주는 함수
- * v_ChangeNextScene()          다음씬으로 넘어가는 함수
- * v_WinText()                  텍스트를 활성화해주는 함수
- */
+/*
+  * - Name: CheckAnswerEpi2.class
+  * - Writer: Yoo Hee-su, Byungkwon Lee
+  * - Content: Hansel and Gretel Epi2 minigame - A script that counts how many puzzle pieces have been matched and moves to the next scene when all are matched.
+  *
+  * - HISTORY
+  * 2021-08-10: Initial development
+  * 2021-08-12: Code standardization and comment processing
+  *
+  * <Variable>
+  * mn_count variable to check how many puzzles have been solved
+  * v_YouWinText Text indicating that the game is over
+  *
+  * <Function>
+  * v_CountAnswer() A function that adds a count variable every time a puzzle is answered.
+  * v_ChangeNextScene() Function to move to the next scene
+  * v_WinText() Function that activates text
+  */
 
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CheckAnswerEpi2 : MonoBehaviour{
-    public int mn_count;
-    public GameObject v_YouWinText;
+     public int mn_count;
+     public GameObject v_YouWinText;
 
-    GameObject mg_SoundManager;
-    void Start(){
-        mn_count = 0;                                                            //퍼즐 개수 변수 0으로 초기화
-        v_YouWinText.SetActive(false);                                           //텍스트 비활성화
-        mg_SoundManager = GameObject.Find("SoundManager");                 // 사운드 매니저 게임오브젝트 연결
-    }
+     GameObject mg_SoundManager;
+     void Start(){
+         mn_count = 0; //Initialize puzzle number variable to 0
+         v_YouWinText.SetActive(false); //disable text
+         mg_SoundManager = GameObject.Find("SoundManager"); // Connect the sound manager game object
+     }
 
-    //퍼즐을 맞출때마다 개수 변수를 더해주는 함수
-    //퍼즐이 모두 맞춰지는지 개수로 확인하고, 다 맞춰지면 다음 씬으로 이동
-    public void v_CountAnswer(){
-        mn_count++;                                                              //개수 하나씩 더해준다
-        Debug.Log(mn_count);
-        if (mn_count == 4){                                                   //맞춰진 퍼즐개수가 9개라면
-            Invoke("v_WinText", 1f);                                             //텍스트 활성화
-            Invoke("v_ChangeNextScene", 3f);                                     //다음씬 불러오기
-        }
-    }
+     //Function that adds a count variable every time you solve a puzzle
+     //Check whether all the puzzles fit together by counting them, and if so, move to the next scene
+     public void v_CountAnswer(){
+         mn_count++; //Add the numbers one by one
+         Debug.Log(mn_count);
+         if (mn_count == 4){ //If the number of puzzles solved is 9
+             Invoke("v_WinText", 1f); //Activate text
+             Invoke("v_ChangeNextScene", 3f); //Load the next scene
+         }
+     }
 
-    //다음 씬으로 이동하는 함수
-    public void v_ChangeNextScene(){
-        SceneManager.LoadScene("1_03H&G");
-    }
+     //Function to move to the next scene
+     public void v_ChangeNextScene(){
+         SceneManager.LoadScene("1_03H&G");
+     }
 
-    //텍스트를 활성화해주는 함수
-    public void v_WinText(){
-        v_YouWinText.SetActive(true);
-        mg_SoundManager.GetComponent<SoundManager>().playSound("Win");     // 게임 끝 버튼 효과음 재생       
-    }
+     //Function to activate text
+     public void v_WinText(){
+         v_YouWinText.SetActive(true);
+         mg_SoundManager.GetComponent<SoundManager>().playSound("Win"); // Play end game button sound effect
+     }
 }
